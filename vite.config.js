8d7acd44f4a8f45802b72a4c9dac88c9e8b71ee1
Pallavi Dandge <pallavi.d@coditude.com>
2025-06-19 17:51:36 +0530

    dist added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Tic-Tac-Teo-Project/',
+  base: '/Tic-Tac-Teo-Project/', // 👈 VERY IMPORTANT!
   plugins: [react()],
 })
