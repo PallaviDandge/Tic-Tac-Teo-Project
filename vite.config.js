1858b0df354c81459719393bb716183d38377b34
Pallavi Dandge <pallavi.d@coditude.com>
2025-06-20 15:21:18 +0530

    config chnge

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Tic-Tac-Teo-Project/', // 👈 VERY IMPORTANT!
+  base: '/Tic-Tac-Teo-Project/', // 👈 GitHub Pages path
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // 👈 increase limit to avoid the warning (default was 500)
+  },
 })
